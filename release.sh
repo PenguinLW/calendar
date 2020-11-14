a2315efdc03fa079d54fb75f141378558db694b0
Kovalsky95 <diana1997525@gmail.com>
2020-11-14 21:27:04 +0800

    PenguinL

--- release.sh
+++ release.sh
@@ -1,4 +1,9 @@
 #git clone -o calendar https://github.com/Kovalsky95/calendar.git
+#git config --global credential.helper cache
+git config --global credential.helper "cache --timeout=3600"
 git add .
 git commit -m PenguinL
 git push --set-upstream calendar master
+
+#git config credential.helper store
+#git config --unset credential.helper
