7fcde888d637a59e79d19978bc4dda259fbaef1b
Kovalsky95 <diana1997525@gmail.com>
2020-11-14 21:14:42 +0800

    PenguinL

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #git clone -o calendar https://github.com/Kovalsky95/calendar.git
-echo Kovalsky95
+
 git add .
 git commit -m PenguinL
-git push --set-upstream calendar master
+echo Kovalsky95 | git push --set-upstream calendar master
