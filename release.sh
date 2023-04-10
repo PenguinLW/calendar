29ea7ab944f069453324aadd5050e3af26991bd6
PenguinLW <diana1997525@gmail.com>
2023-04-10 19:22:44 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #in venv
 #pip install -r req.txt
 pip freeze > req.txt
-#git clone -o calendar https://github.com/Kovalsky95/calendar.git
+#git clone -o calendar https://github.com/PenguinLW/calendar.git
 #git config --global credential.helper cache
 git config --global credential.helper "cache --timeout=25200"
 #git config credential.helper store
